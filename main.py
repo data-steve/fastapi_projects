48017b0677721bfca0c661c1808e57fb1cb7ed87
data-steve <steven.troy.simpson@gmail.com>
2025-04-02 13:18:12 -0400

    hand built hard coded error handling when id isn't in records

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
@@ -37,8 +37,12 @@ def create_posts(post: Post):
     post_dict = post.model_dump()
     post_dict['id'] = randrange(2,100_000_000) 
     my_posts.append(post_dict)
-    return {"da ta": post_dict}
-
+    return {"data": post_dict}
+ 
 @app.get("/posts/{id}")
-def get_post(id: int):
-    return {"data": find_post(id)}
+def get_post(id: int, response: Response):
+    post = find_post(id)
+    if not post:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'error':f'id {id} was not found'}
+    return {"post_detail": post }
