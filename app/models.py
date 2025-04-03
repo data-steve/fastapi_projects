cd99c03013fe463b242ab2eaac4fc0dccb92b959
data-steve <steven.troy.simpson@gmail.com>
2025-04-03 12:11:54 -0400

    added created_at field and new expressions text dependency

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, Boolean, String
+from sqlalchemy.sql.expression import text
+from sqlalchemy import TIMESTAMP, Column, Integer, Boolean, String
 from .database import Base
 
 class Post(Base):
@@ -7,5 +8,5 @@ class Post(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, nullable=True)
-    
+    published = Column(Boolean, server_default='TRUE', nullable=False) 
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
