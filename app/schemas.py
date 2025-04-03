f10a58d42c270b1001ebdc15249b5cac3a42bf99
data-steve <steven.troy.simpson@gmail.com>
2025-04-03 14:11:19 -0400

    added User to model and shema

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,7 @@
-from pydantic import BaseModel
+
+from datetime import datetime
+from xmlrpc.client import boolean
+from pydantic import BaseModel, EmailStr
 from typing import Optional
 
 class PostBase(BaseModel):
@@ -10,3 +13,14 @@ class PostBase(BaseModel):
 
 class PostCreate(PostBase):
     pass
+
+class PostResponse(PostBase):
+    id : int
+    created_at : datetime
+    
+    class Config:
+        from_attributes = True
+        
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
