c119fe99056ee9a8171da74d9367ebe8bf32dc9a
data-steve <steven.troy.simpson@gmail.com>
2025-04-09 16:11:58 -0400

    moved old straight sql ways here

--- app/database.py
+++ app/database.py
@@ -18,3 +18,38 @@ def get_db():
         yield db
     finally:
         db.close()
+        
+        
+
+# import psycopg2
+# from psycopg2.extras import RealDictCursor
+# import time
+# my_posts = [{"title": f"title of post {i}", "content": f"content post {i}", "id": (i-2)} for i in range(10,2,-1)]
+
+# def find_post(id): 
+#     for l in my_posts:
+#         if l['id'] == id:
+#             return l
+
+# def find_index_post(id):
+#     for i, p in enumerate(my_posts):
+#         if p['id']==id:
+#             return i
+
+# version 3
+# import psycopg
+# from psycopg.rows import dict_row
+
+# while True:
+#     try: 
+#         conn = psycopg2.connect("host=localhost dbname=fastapi user=postgres password=postgres",
+#                             #    row_factory=dict_row
+#                             cursor_factory=RealDictCursor)
+#         cursor = conn.cursor()
+#         print("Database connection wa s successful!")
+#         break
+#     except KeyboardInterrupt: 
+#         print("Stopped by user")
+#     except Exception as error:
+#         print(f"Error connecting to database:\n\tError = {error}")
+#         time.sleep(2)
