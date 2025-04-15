9e740726d76f79622a6aa258f0549e825117d0c6
data-steve <steven.troy.simpson@gmail.com>
2025-04-15 16:28:02 -0400

    added functionality to test login user

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,67 +1,32 @@
-from fastapi.testclient import TestClient
 from app import schemas
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from config import settings
-from app.database import get_db, Base
+from .database import client, session
 import pytest
+from jose import jwt
+from config import settings
 
-from app.main import app 
-
-# had to run `PGPASSWORD=postgres psql -h localhost -p 5432 -U postgres -c "CREATE DATABASE fastapi_test;"` to create fastapi_test database
-SQLALCHEMY_DATABASE_URL_TEST = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL_TEST)
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-
-# def override_get_db():
-#     db = TestingSessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
 
 @pytest.fixture
-def session():
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-@pytest.fixture
-def client(session):
-    def override_get_db():
-        try:
-            yield session
-        finally:
-            session.close()
-    app.dependency_overrides[get_db] = override_get_db
-    yield TestClient(app)
-    
-
-
-def test_root(client):
-    res = client.get("/")
-    assert res.json().get("message") == "Hello!"
-    assert res.status_code ==200
+def test_user(client):
+    user_data = {"email":"voldemort@gmail.com", "password":"123"}
+    res = client.post('/users/', json=user_data)
+    assert res.status_code == 201
+    # print(res.json())
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
 
 
 def test_create_users(client):
     res = client.post("/users/", json={"email":"voldemort@gmail.com", "password":"123"})
-    new_user = schemas.UserResponse(**res.json())
+    new_user = schemas.UserResponse(**res.json()) 
     assert new_user.email == "voldemort@gmail.com"
     assert res.status_code==201
     
-    
-# def test_get_users():
-#     res = client.get("/users/", json={"email":"elaine@gmail.com", "password":"123"})
-#     print(res)
-    
-#     client.get()
+
+def test_login_user(client, test_user):
+    res = client.post("/login", data={"username":test_user['email'], "password":test_user['password']})
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token, settings.secret_key, algorithms=[settings.algorithm])
+    assert payload.get("user_id") == test_user['id']
+    assert login_res.token_type == "bearer"
+    assert res.status_code==200
