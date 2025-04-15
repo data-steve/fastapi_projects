9d1d239b9d434659209be297f095afb621b613ba
data-steve <steven.troy.simpson@gmail.com>
2025-04-15 10:37:03 -0400

    practiced testing classes and creating custom Exception classes

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,5 +1,14 @@
 import pytest
-from app.calculations import add, subtract, multiply,divide
+from app.calculations import add, subtract, multiply, divide, BankAccount, InsufficientFunds
+
+@pytest.fixture
+def zero_bank_account():
+    return BankAccount()
+
+@pytest.fixture
+def bank_account():
+    return BankAccount(50)
+
 
 @pytest.mark.parametrize("num1, num2, expected",[
     (3,2,5),
@@ -33,3 +42,38 @@ def test_multiply(num1, num2, expected):
 ])     
 def test_divide(num1, num2, expected):
     assert divide(num1, num2)==expected
+    
+
+def test_bank_set_initial_amount(bank_account):
+    assert bank_account.balance==50
+    
+def test_bank_default_amount(zero_bank_account):
+    assert zero_bank_account.balance ==0
+    
+def test_bank_withdraw(bank_account):
+    bank_account.withdraw(15)
+    assert bank_account.balance == 35
+    
+def test_bank_deposit(bank_account):
+    bank_account.deposit(15)
+    assert bank_account.balance == 65
+    
+def test_bank_collect_interest(bank_account):
+    bank_account.collect_interest()
+    assert int(bank_account.balance) == 55
+
+@pytest.mark.parametrize("deposit, withdrew, expected",[
+        (50, 24, 26),
+        (1500, 1200, 300),
+        (110, 100, 10)
+])
+def test_bank_transaction(zero_bank_account, deposit, withdrew, expected):
+    zero_bank_account.deposit(deposit)
+    zero_bank_account.withdraw(withdrew)
+    assert zero_bank_account.balance==expected
+    
+    
+def test_insufficient_funds(bank_account):
+    with pytest.raises(InsufficientFunds):
+        bank_account.withdraw(200)
+    
