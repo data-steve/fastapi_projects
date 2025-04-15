b100242bcb01c9094cd71096aaa5de5eda47e978
data-steve <steven.troy.simpson@gmail.com>
2025-04-15 15:38:47 -0400

    moved TestClient into a fixture that so that db and client control because parameters that we can we can access in any test we write. and htey get torn down and build new each test run

--- tests/test_users.py
+++ tests/test_users.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config import settings
 from app.database import get_db, Base
+import pytest
 
 from app.main import app 
 
@@ -14,29 +15,45 @@ SQLALCHEMY_DATABASE_URL_TEST = f'postgresql://{settings.database_username}:{sett
 engine = create_engine(SQLALCHEMY_DATABASE_URL_TEST)
 
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base.metadata.create_all(bind=engine)
 
 
-def override_get_db():
+
+# def override_get_db():
+#     db = TestingSessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
+@pytest.fixture
+def session():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
     db = TestingSessionLocal()
     try:
         yield db
     finally:
         db.close()
 
+@pytest.fixture
+def client(session):
+    def override_get_db():
+        try:
+            yield session
+        finally:
+            session.close()
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
+    
 
-app.dependency_overrides[get_db] = override_get_db
 
-
-client = TestClient(app)
-
-def test_root():
+def test_root(client):
     res = client.get("/")
     assert res.json().get("message") == "Hello!"
     assert res.status_code ==200
 
 
-def test_create_users():
+def test_create_users(client):
     res = client.post("/users/", json={"email":"voldemort@gmail.com", "password":"123"})
     new_user = schemas.UserResponse(**res.json())
     assert new_user.email == "voldemort@gmail.com"
