c33323f3f3ac40bf838090cf2f65b67356b26fbe
data-steve <steven.troy.simpson@gmail.com>
2025-04-15 10:01:42 -0400

    parameterized pytest

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,14 +1,35 @@
+import pytest
 from app.calculations import add, subtract, multiply,divide
 
-def test_add():
+@pytest.mark.parametrize("num1, num2, expected",[
+    (3,2,5),
+    (7,9,16),
+    (14,3, 17)
+])
+def test_add(num1, num2, expected):
     print("testing add function")  
-    assert add(3, 5) ==8
-    
-def test_subtract():
-    assert subtract(5, 3) ==2
-    
-def test_multiply():
-    assert multiply(3,4)== 12
-    
-def test_divide():
-    assert divide(12,4)==3
+    assert add(num1, num2) ==expected
+
+@pytest.mark.parametrize("num1, num2, expected",[
+    (3,2,1),
+    (7,9,-2),
+    (14,3, 11)
+])  
+def test_subtract(num1, num2, expected):
+    assert subtract(num1, num2) ==expected
+
+@pytest.mark.parametrize("num1, num2, expected",[
+    (3,2,6),
+    (14,2,28),
+    (3,3, 9)
+])   
+def test_multiply(num1, num2, expected):
+    assert multiply(num1, num2) ==expected
+
+@pytest.mark.parametrize("num1, num2, expected",[
+    (6,2,3),
+    (14,2,7),
+    (3,3, 1)
+])     
+def test_divide(num1, num2, expected):
+    assert divide(num1, num2)==expected
